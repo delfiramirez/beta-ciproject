d6bda88b0fccfdfadd49b54566b660ddd4adb83c
Segonquart Studio {public repositories} <delfin@segonquart.net>
2016-10-20 12:02:54 +0200

    v04 update, app.css

--- 04/js/utils.js
+++ 04/js/utils.js
@@ -6,25 +6,3 @@ if (matchMedia(query)) {
  
 }
 
-let supports = {};
-let style = document.body.style;
-
-export default {
-  flexbox: function() {
-    return supports.flexbox || (supports.flexbox = ('flexBasis' in style ||
-        'msFlexAlign' in style || 'webkitBoxDirection' in style));
-  }
-};
-
-
-// Add class on browsers that don't supports flexbox. ES6-ES7
-
-if (!supports.flexbox()) {
-  let div = document.createElement('div');
-  div.className = 'Error';
-  div.innerHTML = `Your browser does not support Flexbox.`;
-
-  document.body.insertBefore(div, document.body.firstChild);
-}
-
-
